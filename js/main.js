a6d9d336b3495e24654d6a431a24aa2902f92deb
Mark-dev30 <fronterremarco@gmail.com>
2023-01-09 15:23:20 +0100

    completed exercise

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        listEmail: [],
+      }
+    },
+    methods: {
+        generateEmail(){
+            for(let i=0; i< 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                this.listEmail.push(response.data.response);
+            }
+            );
+        }
+        }
+    },
+  }).mount('#app')
+  
+
