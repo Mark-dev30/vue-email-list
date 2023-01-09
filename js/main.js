f263394ac5e1841f18cb97ef95249caedb038897
Mark-dev30 <fronterremarco@gmail.com>
2023-01-09 15:43:59 +0100

    completed bonus

--- js/main.js
+++ js/main.js
@@ -6,13 +6,23 @@
     data() {
       return {
         listEmail: [],
+        listEmailclick: []
       }
+    },
+    mounted() {
+        for(let i=0; i< 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+            this.listEmail.push(response.data.response);
+            console.log(this.listEmail)
+        }
+        );
+    }
     },
     methods: {
         generateEmail(){
             for(let i=0; i< 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-                this.listEmail.push(response.data.response);
+                this.listEmailclick.push(response.data.response);
             }
             );
         }
